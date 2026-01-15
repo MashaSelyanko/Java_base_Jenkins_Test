8555806694840e64141a1819684684d555ccc232
svasenkov <aanher@gmail.com>
2026-01-15 19:26:05 +0100

    added base remote driver config

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -13,6 +13,10 @@ public class RegistrationTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+//        Configuration.browserVersion = "128.0";
+//        Configuration.browserVersion = "130.0";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
